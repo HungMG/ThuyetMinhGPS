f333dbfaa4661348240a6223eebf1f89f0c02b1f
sangm <sangm@HUNGMG>
2026-03-15 13:29:43 +0700

    database thử

---
+++ TourGuideApp/Models/POI.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+
+namespace TourGuideApp.Models
+{
+    [Table("POI")]
+    public class POI
+    {
+        [PrimaryKey] // Nếu bạn muốn DB tự tạo ID thì thêm [AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public int TriggerRadius { get; set; }
+
+        public string Description { get; set; }
+
+        public string AudioSource { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public bool IsVisited { get; set; }
+    }
+}
