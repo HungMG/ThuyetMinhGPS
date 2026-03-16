5822b2e3fdc1de4c19143c07e93650ea7427620c
lechauky <167744661+lechauky@users.noreply.github.com>
2026-03-16 22:59:38 +0700

    add settings page

---
+++ TourGuideApp/Views/SettingsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourGuideApp.Views
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
