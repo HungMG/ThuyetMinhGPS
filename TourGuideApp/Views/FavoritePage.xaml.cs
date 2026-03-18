23f4b6c299eb7bc4c7a88c06af63ad6f25a95818
lechauky <167744661+lechauky@users.noreply.github.com>
2026-03-18 22:48:29 +0700

    thêm điểm yêu thích

---
+++ TourGuideApp/Views/FavoritePage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TourGuideApp.Views
+{
+    public partial class FavoritePage : ContentPage
+    {
+        public FavoritePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
