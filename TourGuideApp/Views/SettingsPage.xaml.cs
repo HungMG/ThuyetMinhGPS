7fc82c414d7450fe069a9c2eed5f36f686ced951
lechauky <167744661+lechauky@users.noreply.github.com>
2026-03-25 00:25:25 +0700

    cập nhật giao diện

--- TourGuideApp/Views/SettingsPage.xaml.cs
+++ TourGuideApp/Views/SettingsPage.xaml.cs
@@ -10,5 +10,19 @@
         {
             await Navigation.PushAsync(new OfflinePage());
         }
+        private async void OnLogoutTapped(object sender, EventArgs e)
+        {
+            bool confirm = await DisplayAlert("Đăng xuất", "Bạn có chắc muốn đăng xuất?", "Có", "Không");
+
+            // if (confirm)
+            // {
+                // TODO: Xóa dữ liệu đăng nhập (token, user...)
+                // Preferences.Remove("user");
+
+                // Quay về trang login
+                // await Navigation.PushAsync(new LoginPage());
+            // }
+        }
+
     }
 }
