f1e247dec09e8725c3f8946d8f7fd18ca7e25a2b
HungMG <sangm1238@gmail.com>
2026-04-02 23:52:36 +0700

    gộp web và app chung 1 git

---
+++ TourGuideAdmin/TourGuideAdmin/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TourGuideAdmin.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // 👇 Đổi dòng này thành DbSet<POI>
+        public DbSet<POI> POIs { get; set; }
+        public DbSet<Tour> Tours { get; set; }
+    }
+}
